e7f9c5eb97bf8f838e55c5550fd7e397edfbf3d0
Ben Davies <ben.davies@ourscienceistight.com>
2020-10-08 08:29:35 +0100

    Added WAN egress qos fix

--- fields/main.go
+++ fields/main.go
@@ -260,6 +260,14 @@ func main() {
 				f.OmitEmpty = true
 				return nil
 			}
+		case "NetworkConf":
+			resource.FieldProcessor = func(name string, f *FieldInfo) error {
+				switch name {
+				case "WANEgressQOS":
+					f.FieldType = "string"
+				}
+				return nil
+			}
 		case "SettingUsg":
 			resource.FieldProcessor = func(name string, f *FieldInfo) error {
 				if strings.HasSuffix(name, "Timeout") && name != "ArpCacheTimeout" {
