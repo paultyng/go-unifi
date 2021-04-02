ac8d0c56593e4ae8ce1ac35638566571c569f2c7
Kurt McAlpine <kurt@linux.com>
2021-04-02 17:51:35 +1300

    Set key to mgmt for updateSettingMgmt api

--- unifi/setting_mgmt.go
+++ unifi/setting_mgmt.go
@@ -9,5 +9,6 @@ func (c *Client) GetSettingMgmt(ctx context.Context, site string) (*SettingMgmt,
 }
 
 func (c *Client) UpdateSettingMgmt(ctx context.Context, site string, d *SettingMgmt) (*SettingMgmt, error) {
+	d.Key = "mgmt"
 	return c.updateSettingMgmt(ctx, site, d)
 }
