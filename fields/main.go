3d383e172be2bc8b65ec3ffca45194881d29fb8c
Joshua Spence <josh@joshuaspence.com>
2021-09-16 12:17:42 +1000

    Add `AdoptDevice` and `ForgetDevice` methods

--- fields/main.go
+++ fields/main.go
@@ -145,6 +145,8 @@ func NewResource(structName string, resourcePath string) *Resource {
 		baseType.Fields[" Key"] = NewFieldInfo("Key", "key", "string", "", false, false, "")
 	case resource.StructName == "Device":
 		baseType.Fields[" MAC"] = NewFieldInfo("MAC", "mac", "string", "", true, false, "")
+		baseType.Fields["Adopted"] = NewFieldInfo("Adopted", "adopted", "bool", "", false, false, "")
+		baseType.Fields["State"] = NewFieldInfo("State", "state", "int", "", false, false, "")
 	case resource.StructName == "User":
 		baseType.Fields[" IP"] = NewFieldInfo("IP", "ip", "string", "non-generated field", true, false, "")
 		baseType.Fields[" DevIdOverride"] = NewFieldInfo("DevIdOverride", "dev_id_override", "int", "non-generated field", true, false, "")
--- unifi/device.go
+++ unifi/device.go
@@ -2,6 +2,22 @@ package unifi
 
 import (
 	"context"
+	"fmt"
+)
+
+const (
+	DEVICE_STATE_UNKNOWN           = 0
+	DEVICE_STATE_CONNECTED         = 1
+	DEVICE_STATE_PENDING           = 2
+	DEVICE_STATE_FIRMWARE_MISMATCH = 3
+	DEVICE_STATE_UPGRADING         = 4
+	DEVICE_STATE_PROVISIONING      = 5
+	DEVICE_STATE_HEARTBEAT_MISSED  = 6
+	DEVICE_STATE_ADOPTING          = 7
+	DEVICE_STATE_DELETING          = 8
+	DEVICE_STATE_INFORM_ERROR      = 9
+	DEVICE_STATE_ADOPT_FAILED      = 10
+	DEVICE_STATE_ISOLATED          = 11
 )
 
 func (c *Client) ListDevice(ctx context.Context, site string) ([]Device, error) {
@@ -38,4 +54,47 @@ func (c *Client) GetDevice(ctx context.Context, site, id string) (*Device, error
 	}
 
 	return nil, &NotFoundError{}
+}
+
+func (c *Client) AdoptDevice(ctx context.Context, site, mac string) error {
+	reqBody := struct {
+		Cmd string `json:"cmd"`
+		MAC string `json:"mac"`
+	}{
+		Cmd: "adopt",
+		MAC: mac,
+	}
+
+	var respBody struct {
+		Meta meta `json:"meta"`
+	}
+
+	err := c.do(ctx, "POST", fmt.Sprintf("s/%s/cmd/devmgr", site), reqBody, &respBody)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) ForgetDevice(ctx context.Context, site, mac string) error {
+	reqBody := struct {
+		Cmd  string   `json:"cmd"`
+		MACs []string `json:"macs"`
+	}{
+		Cmd:  "delete-device",
+		MACs: []string{mac},
+	}
+
+	var respBody struct {
+		Meta meta     `json:"meta"`
+		Data []Device `json:"data"`
+	}
+
+	err := c.do(ctx, "POST", fmt.Sprintf("s/%s/cmd/sitemgr", site), reqBody, &respBody)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
